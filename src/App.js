97680a2aae524ec5069a9dacfe8889174e026dbb
Kota Watanabe <kota3316@gmail.com>
2019-06-18 13:21:48 -0700

    create Rank component and design background with particles

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
 import React from 'react';
+import Particles from 'react-particles-js';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
 import './App.css';
+
+const particlesOptions = {
+  particles:{
+    number:{
+      value:80,
+      density:{
+        enable: true,
+        value_area:1000
+      }
+    }
+  }
+}
 
 function App() {
   return (
     <div className="App">
+      <Particles className="particles"
+        params={particlesOptions}
+      />
       <Navigation />
       <Logo />
+      <Rank />
       <ImageLinkForm />
       {/*
       <FaceRecognition />*/}
