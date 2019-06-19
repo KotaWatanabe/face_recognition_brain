c3fbacba4c2d78b602ab8d1624a53d14b4940b24
Kota Watanabe <kota3316@gmail.com>
2019-06-19 14:46:44 -0700

    make navbar dynamically changeable

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Signin from './components/Signin/Signin';
+import Register from './components/Register/Register';
 import Rank from './components/Rank/Rank';
 import './App.css';
 
@@ -32,7 +33,8 @@ class App extends Component {
       input:"",
       imageUrl:"",
       box:{},
-      route: 'signin'
+      route: 'signin',
+      isSignedIn: false
     }
   }
 calculateFaceLocation = (data) => {
@@ -67,6 +69,11 @@ displayFaceBox = (box) => {
   }
 
   onRouteChange = (route) => {
+    if(route === "signout"){
+      this.setState({isSignedIn: false})
+    }else if(route === 'home'){
+      this.setState({isSignedIn: true})
+    }
     this.setState({route: route});
   }
 
@@ -76,10 +83,9 @@ displayFaceBox = (box) => {
         <Particles className="particles"
           params={particlesOptions}
         />
-        <Navigation onRouteChange = {this.onRouteChange} />
-        { this.state.route === 'signin' 
-          ? <Signin onRouteChange = {this.onRouteChange}/>
-          : <div>
+        <Navigation isSignedIn = {this.state.isSignedIn} onRouteChange = {this.onRouteChange}  />
+        { this.state.route === 'home' 
+          ? <div>
               <Logo />
               <Rank />
               <ImageLinkForm 
@@ -89,6 +95,12 @@ displayFaceBox = (box) => {
               {
               <FaceRecognition box={this.state.box} imageUrl=     {this.state.imageURl}/>}
             </div>
+          :(
+            this.state.route === 'signin'
+            ? <Signin onRouteChange = {this.onRouteChange}/>
+            : <Register onRouteChange = {this.onRouteChange}/>
+           )
+          
         }
       </div>
     );
