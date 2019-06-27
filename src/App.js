70e3446f63c059391c9905d4e2595f45ca090065
Kota Watanabe <kota3316@gmail.com>
2019-06-27 10:24:53 -0700

    fix state. create initial state

--- src/App.js
+++ src/App.js
@@ -26,23 +26,25 @@ const particlesOptions = {
   }
 }
 
+const initialState = {
+  input:"",
+  imageUrl:"",
+  box:{},
+  route: 'signin',
+  isSignedIn: false,
+  user:{
+    id: '',
+    name: '',
+    email: '',
+    entries: 0,
+    joined: ''
+  }
+}
+
 class App extends Component {
   constructor(){
     super();
-    this.state={
-      input:"",
-      imageUrl:"",
-      box:{},
-      route: 'signin',
-      isSignedIn: false,
-      user:{
-        id: '',
-        name: '',
-        email: '',
-        entries: 0,
-        joined: ''
-      }
-    }
+    this.state = initialState;
   }
 
   loadUser =(data)=>{
@@ -84,7 +86,7 @@ displayFaceBox = (box) => {
           this.state.input)
         .then(response => {
           if(response){
-            fetch('http*//localhost:3001/image', {
+            fetch('http://localhost:3001/image', {
              method: 'put',
               headers:{'Content-Type': 'application/json'},
               body: JSON.stringify({
@@ -95,6 +97,7 @@ displayFaceBox = (box) => {
               .then(count => {
                 this.setState(Object.assign(this.state.user, { entries: count }))
               })
+              .catch(console.log)
           }
           this.displayFaceBox(this.calculateFaceLocation(response))
         })
@@ -103,7 +106,7 @@ displayFaceBox = (box) => {
 
   onRouteChange = (route) => {
     if(route === "signout"){
-      this.setState({isSignedIn: false})
+      this.setState(initialState)
     }else if(route === 'home'){
       this.setState({isSignedIn: true})
     }
